f9f2b714c55278e1a3d433c93e2182bb5881ec03
김태훈 <kimtaehun@gimtaehun-ui-MacBookPro.local>
2022-08-11 17:56:35 +0900

    settings-#1: TDD tutorial

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Param,
+  Req,
+  Res,
+  Response,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //API WELCOME PAGE
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const apiDocs = `http://localhost:${process.env.PORT}/api-docs`;
+    return `<div><H1>ImMall Backend API</H1></div>Welcome to visit the ImMall Backend API Home.<br>Please click <a href=${apiDocs}>here</a> want to see the api-docs.`;
+  }
+
+  @Get('test')
+  getTest(): string {
+    return 'test';
   }
 }
---
+++ server/src/user/user.controller.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let controller: UserController;
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = ['test'];
+      jest.spyOn(service, 'findAll').mockImplementation(() => result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+  }
+})
