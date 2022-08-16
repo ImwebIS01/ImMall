c0d4d4504a08dfe62913b9d47320ee06e4909855
김태훈 <kimtaehun@gimtaehun-ui-MacBookPro.local>
2022-08-17 07:58:39 +0900

    PI01-125 유저 기본 CRUD 적용

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -3,18 +3,17 @@ import { DatabaseService } from 'src/database/database.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+// input-output dto 모두 있어야 함
+
 @Injectable()
 export class UserService {
-  constructor(
-    private readonly databaseService: DatabaseService
-  ){}
-  
-  async create(createUserDto: CreateUserDto) {
+  constructor(private readonly databaseService: DatabaseService) {}
 
+  async create(createUserDto: CreateUserDto) {
     try {
       const { username, email, password }: CreateUserDto = createUserDto;
       const newUser = await this.databaseService.getConnection().query(`
-      INSERT INTO test.user 
+      INSERT INTO user 
       (username, email, password) 
       VALUES ("${username}","${email}","${password}");
       `);
@@ -25,22 +24,60 @@ export class UserService {
   }
 
   async findAll() {
-    return this.databaseService.getConnection().query(`
-    SELECT * FROM user ORDER BY create_time DESC;
-    `)
+    try {
+    } catch (error) {}
+    const users = await this.databaseService.getConnection().query(`
+    SELECT * FROM user ORDER BY created_time DESC;
+    `);
+    return users[0];
   }
 
   async findOne(id: number) {
-    return this.databaseService.getConnection().query(`
-    SELECT * FROM user WHERE id = ${id} ORDER BY create_time DESC;
-    `)
+    try {
+      const user = await this.databaseService.getConnection().query(`
+      SELECT * 
+      FROM user 
+      WHERE id = ${id};`);
+      return user[0][0];
+    } catch (error) {
+      throw error;
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    try {
+      const user = await this.findOne(id);
+      const username = updateUserDto.username
+        ? updateUserDto.username
+        : user.username;
+      const email = updateUserDto.email ? updateUserDto.email : user.email;
+      const password = updateUserDto.password
+        ? updateUserDto.password
+        : user.password;
+
+      await this.databaseService.getConnection().query(`
+      UPDATE user
+      SET
+      username='${username}',
+      email='${email}',
+      password='${password}'
+
+      WHERE id=${id};
+      `);
+      return this.findOne(id);
+    } catch (error) {
+      throw error;
+    }
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} user`;
+    try {
+      return await this.databaseService.getConnection().query(`
+      DELETE from user
+      WHERE id=${id}
+      `);
+    } catch (error) {
+      throw error;
+    }
   }
 }
