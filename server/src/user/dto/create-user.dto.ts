3c9c5104cd1118a92f3210a35d5f6007c04614c0
김태훈 <kimtaehun@gimtaehun-ui-MacBookPro.local>
2022-08-18 15:05:16 +0900

    feat: 유저 DTO 수정

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -4,18 +4,18 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @MinLength(4)
   @MaxLength(20)
-  @ApiProperty({
-    description: '닉네임(4~20자, 영문 대소문자, 한글, 숫자만 가능)',
-  })
-  readonly username: string;
+  readonly name: string;
 
   @IsEmail()
-  @ApiProperty({ description: '이메일' })
   readonly email: string;
 
   @IsString()
   @MinLength(4)
   @MaxLength(20)
-  @ApiProperty({ description: '비밀번호(4~20자)' })
-  readonly password: string;
+  readonly passwd: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  readonly callNum: string;
 }
