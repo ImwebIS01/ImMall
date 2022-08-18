208ee625a20df340e9ded3099b32270d0ae21566
김태훈 <kimtaehun@gimtaehun-ui-MacBookPro.local>
2022-08-18 14:32:56 +0900

    feat: 유저 스키마 초안 완성

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -2,24 +2,39 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 interface UserInterface {
-  readonly id: number;
-  readonly username: string;
+  readonly idx: number;
+  readonly code: string;
+  readonly name: string;
   readonly email: string;
-  readonly password: string;
-  readonly created_time: Date;
+  readonly passwd: string;
+  readonly jointime: Date;
+  readonly callnum: string;
 }
 
 export class User implements UserInterface {
-  readonly id: number;
-  readonly username: string;
+  readonly idx: number;
+  readonly code: string;
+  readonly name: string;
   readonly email: string;
-  readonly password: string;
-  readonly created_time: Date;
+  readonly passwd: string;
+  readonly jointime: Date;
+  readonly callnum: string;
 
-  constructor(id: number, username: string, email: string, password: string) {
-    this.id = id;
-    this.username = username;
+  constructor(
+    idx: number,
+    code: string,
+    name: string,
+    email: string,
+    passwd: string,
+    jointime: Date,
+    callnum: string
+  ) {
+    this.idx = idx;
+    this.code = code;
+    this.name = name;
     this.email = email;
-    this.password = password;
+    this.passwd = passwd;
+    this.jointime = jointime;
+    this.callnum = callnum;
   }
 }
