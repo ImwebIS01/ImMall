738e8c032a7351a2598a6ecdf7bee6195fa55bbc
sokojh <sokojiho2@gmail.com>
2022-08-29 13:17:36 +0900

    fix:repo-service 병합

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -3,11 +3,10 @@ import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { DatabaseService } from 'src/database/database.service';
-import { OrderRepository } from './order.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [OrderController],
-  providers: [OrderService, DatabaseService, OrderRepository],
+  providers: [OrderService, DatabaseService],
 })
 export class OrderModule {}
--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -1,102 +1,199 @@
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { OrderRepository } from './order.repository';
 import { DatabaseService } from 'src/database/database.service';
 import {
-  ConflictException,
   Inject,
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
-import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrderService {
-  constructor(
-    private readonly databaseService: DatabaseService,
-    private readonly orderRepository: OrderRepository
-  ) {}
-  async register(createOrderDto: CreateOrderDto): Promise<boolean> {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  /**
+   * 주문서 생성 함수
+   * @param createOrderDto
+   * @returns
+   */
+  async register(createOrderDto: CreateOrderDto) {
     try {
-      await this.orderRepository.create(createOrderDto);
-      return true;
+      const {
+        code,
+        order_no,
+
+        site_code,
+        user_code,
+
+        updated_time,
+
+        created_time,
+
+        delivered_time,
+
+        post_number,
+
+        receiver_name,
+
+        receiver_address,
+
+        receiver_phone,
+
+        receiver_phone2,
+
+        total_price,
+      }: CreateOrderDto = createOrderDto;
+      return await this.databaseService.query(`
+      INSERT INTO testA.order(
+        code,
+
+        order_no,
+
+        site_code,
+
+        user_code,
+
+        updated_time,
+
+        created_time,
+
+        delivered_time,
+
+        post_number,
+
+        receiver_name,
+
+        receiver_address,
+
+        receiver_phone,
+
+        receiver_phone2,
+
+        total_price)
+        VALUES('${code}',
+        '${order_no}',
+        '${site_code}',
+        '${user_code}',
+        '${updated_time}',
+        '${created_time}',
+        '${delivered_time}',
+        '${post_number}',
+        '${receiver_name}',
+        '${receiver_address}',
+        '${receiver_phone}',
+        '${receiver_phone2}',
+        '${total_price}')
+        `);
     } catch (error) {
       throw error;
     }
   }
-
+  /**
+   * 주문 전체 확인 함수
+   * @returns 생성일자 기준으로 전체 주문을 배열로 줍니다.
+   */
   async getAll(): Promise<any> {
     try {
-      return this.orderRepository.findAll();
+      const orders = await this.databaseService.query(
+        `SELECT * FROM testA.order ORDER BY created_time DESC`
+      );
+      return orders[0];
     } catch (error) {
       throw error;
     }
   }
-
-  async getOne(id: number): Promise<Order> {
+  async getOne(id: number): Promise<CreateOrderDto> {
     try {
-      return this.orderRepository.findOne(id);
+      const order = await this.databaseService.query(` SELECT * 
+      FROM testA.order 
+      WHERE id = ${id};`);
+      return order[0][0];
     } catch (error) {
       throw error;
     }
   }
-
+  /**
+   * 주문 업데이트 함수
+   * @param id 주문 idx 값
+   * @param updateOrderDto
+   */
   async setOne(id: number, updateOrderDto: UpdateOrderDto) {
     try {
-      const order: Order = await this.orderRepository.findOne(id);
+      const order: UpdateOrderDto = await this.getOne(id);
 
       console.log(updateOrderDto);
-      const code = updateOrderDto.code ? updateOrderDto.code : order.code;
-      const siteCode = updateOrderDto.siteCode
-        ? updateOrderDto.siteCode
-        : order.siteCode;
-      const orderNumber = updateOrderDto.orderNumber
-        ? updateOrderDto.orderNumber
-        : order.orderNumber;
-      const ordererEmail = updateOrderDto.ordererEmail
-        ? updateOrderDto.ordererEmail
-        : order.ordererEmail;
-      const ordererName = updateOrderDto.ordererName
-        ? updateOrderDto.ordererName
-        : order.ordererName;
-      const ordererCall = updateOrderDto.ordererCall
-        ? updateOrderDto.ordererCall
-        : order.ordererCall;
-      const ordererCall2 = updateOrderDto.ordererCall2
-        ? updateOrderDto.ordererCall2
-        : order.ordererCall2;
-      const orderTime = updateOrderDto.orderTime
-        ? updateOrderDto.orderTime
-        : order.orderTime;
-      const completeTime = updateOrderDto.completeTime
-        ? updateOrderDto.completeTime
-        : order.completeTime;
-      const count = updateOrderDto.count ? updateOrderDto.count : order.count;
-      const price = updateOrderDto.price ? updateOrderDto.price : order.price;
 
-      const newOrder: Order = new Order(
-        id,
-        code,
-        siteCode,
-        orderNumber,
-        ordererEmail,
-        ordererName,
-        ordererCall,
-        ordererCall2,
-        orderTime,
-        completeTime,
-        count,
-        price
-      );
-      await this.orderRepository.update(newOrder);
+      const code = updateOrderDto.code ? updateOrderDto.code : order.code;
+      const order_no = updateOrderDto.order_no
+        ? updateOrderDto.order_no
+        : order.order_no;
+      const site_code = updateOrderDto.site_code
+        ? updateOrderDto.site_code
+        : order.site_code;
+      const user_code = updateOrderDto.user_code
+        ? updateOrderDto.user_code
+        : order.user_code;
+      const created_time = updateOrderDto.created_time
+        ? updateOrderDto.created_time
+        : order.created_time;
+      const updated_time = updateOrderDto.updated_time
+        ? updateOrderDto.updated_time
+        : order.updated_time;
+      const delivered_time = updateOrderDto.delivered_time
+        ? updateOrderDto.delivered_time
+        : order.delivered_time;
+      const post_number = updateOrderDto.post_number
+        ? updateOrderDto.post_number
+        : order.post_number;
+      const receiver_name = updateOrderDto.receiver_name
+        ? updateOrderDto.receiver_name
+        : order.receiver_name;
+      const receiver_address = updateOrderDto.receiver_address
+        ? updateOrderDto.receiver_address
+        : order.receiver_address;
+      const receiver_phone = updateOrderDto.receiver_phone
+        ? updateOrderDto.receiver_phone
+        : order.receiver_phone;
+      const receiver_phone2 = updateOrderDto.receiver_phone2
+        ? updateOrderDto.receiver_phone2
+        : order.receiver_phone2;
+      const total_price = updateOrderDto.total_price
+        ? updateOrderDto.total_price
+        : order.total_price;
+
+      return this.databaseService.query(`
+      UPDATE testA.order
+      SET
+      code='${code}',
+      order_no='${order_no}',
+      site_code='${site_code}',
+      user_code='${user_code}',
+      created_time='${created_time}',
+      updated_time='${updated_time}',
+      delivered_time='${delivered_time}',
+      post_number='${post_number}',
+      receiver_name='${receiver_name}',
+      receiver_address='${receiver_address}',
+      receiver_phone='${receiver_phone}',
+      receiver_phone2='${receiver_phone2}',
+      total_price= '${total_price}'
+      WHERE id=${id};
+      `);
     } catch (error) {
       throw error;
     }
   }
-
+  /**
+   * 주문 삭제 함수
+   * @param id idx값으로 찾습니다.
+   * @returns 리턴값은 없습니다.
+   */
   async remove(id: number) {
     try {
-      await this.orderRepository.remove(id);
+      await this.databaseService.query(`
+      DELETE from testA.order
+      WHERE id=${id}
+      `);
     } catch (error) {
       throw error;
     }
