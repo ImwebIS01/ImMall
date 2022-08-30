6e8e550888603dc77f23d276b3726ffbb89b130f
김태훈 <kimtaehun@gimtaehun-ui-MacBookPro.local>
2022-08-30 15:46:35 +0900

    chore: 멤버쉽 변수명 수정

--- server/src/mock-data.ts
+++ server/src/mock-data.ts
@@ -5,18 +5,18 @@ export const Memberships: Array<Object> = new Array(
     idx: 1,
     code: 'test_code_number1',
     level: 1,
-    pointRage: 1,
+    point_rage: 1,
   },
   {
     idx: 2,
     code: 'test_code_number2',
     level: 2,
-    pointRage: 2,
+    point_rage: 2,
   },
   {
     idx: 3,
     code: 'test_code_number3',
     level: 3,
-    pointRage: 3,
+    point_rage: 3,
   }
 );
