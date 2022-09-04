85655300e48b777fc84f4cfff2faf1956cf1397d
sokojh <sokojiho2@gmail.com>
2022-09-04 17:06:34 +0900

    feat: 주문 CRUD 완성

--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -2,70 +2,51 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GetOrderDto } from './dto/get-order.dto';
 
 @Injectable()
 export class OrderService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly databaseService: DatabaseService
+  ) {}
 
   /**
    * 주문서 생성 함수
    * @param createOrderDto
-   * @returns
+   * @returns boolean 값으로 리턴 'true'/'false'
    */
-  async register(createOrderDto: CreateOrderDto) {
+  async create(createOrderDto: CreateOrderDto): Promise<boolean> {
     try {
-      const {
+      const con = await this.databaseService.getConnection();
+      const code = await this.databaseService.genCode();
+      await con.query(`
+      INSERT INTO ${this.configService.get('DB_NAME')}.order(
         code,
         order_no,
-
         site_code,
         user_code,
         post_number,
-
         receiver_name,
-
         receiver_address,
-
         receiver_phone,
-
-        receiver_phone2,
-        status,
-
-        total_price,
-      }: CreateOrderDto = createOrderDto;
-      return await this.databaseService.query(`
-      INSERT INTO testA.order(
-        code,
-
-        order_no,
-
-        site_code,
-
-        user_code,
-
-        post_number,
-
-        receiver_name,
-
-        receiver_address,
-
-        receiver_phone,
-
         receiver_phone2,
         status,
         total_price)
         VALUES('${code}',
-        '${order_no}',
-        '${site_code}',
-        '${user_code}',
-        '${post_number}',
-        '${receiver_name}',
-        '${receiver_address}',
-        '${receiver_phone}',
-        '${receiver_phone2}',
-        '${status}',
-        '${total_price}')
+        '${createOrderDto.order_no}',
+        '${createOrderDto.site_code}',
+        '${createOrderDto.user_code}',
+        '${createOrderDto.post_number}',
+        '${createOrderDto.receiver_name}',
+        '${createOrderDto.receiver_address}',
+        '${createOrderDto.receiver_phone}',
+        '${createOrderDto.receiver_phone2}',
+        '${createOrderDto.status}',
+        '${createOrderDto.total_price}')
         `);
+      return true;
     } catch (error) {
       throw error;
     }
@@ -74,14 +55,14 @@ export class OrderService {
    * 주문 전체 확인 함수
    * @returns 생성일자 기준으로 전체 주문을 배열로 줍니다.
    */
-  async getAll(): Promise<any> {
+  async findAll(): Promise<any> {
     try {
-      const orders = await this.databaseService.query(
+      const con = await this.databaseService.getConnection();
+      const ordersRowData = await con.query(
         `SELECT * 
-        FROM testA.order 
-        ORDER BY created_time DESC`
+        FROM ${this.configService.get('DB_NAME')}.order;`
       );
-      return orders[0];
+      return ordersRowData[0];
     } catch (error) {
       throw error;
     }
@@ -89,102 +70,93 @@ export class OrderService {
 
   /**
    * 주문 하나 불러오는 함수
-   * @param idx 주문 idx 값
+   * @param code 주문 code 값
    * @returns 하나값만 불러와줌
    */
-  async getOne(idx: number): Promise<CreateOrderDto> {
+  async findOne(code: string): Promise<GetOrderDto> {
     try {
-      const order = await this.databaseService.query(`
+      const con = await this.databaseService.getConnection();
+      const orderRowData = await con.query(`
       SELECT * 
-      FROM testA.order 
-      WHERE id = ${idx};`);
-      return order[0][0];
+      FROM ${this.configService.get('DB_NAME')}.order 
+      WHERE code = "${code}"`);
+      //Promise에서 리턴타입으로 Dto를 검사하고 있는데 다시 검사하는 로직인가용?
+      //제가 잘못 이해한거라면 설명 부탁드립니다!ㅎㅎ
+      //const order: GetOrderDto = orderRowData[0][0]; <<
+      return orderRowData[0][0];
     } catch (error) {
       throw error;
     }
   }
   /**
    * 주문 업데이트 함수
-   * @param idx 주문 idx 값
+   * @param code 주문 code 값
    * @param updateOrderDto
    */
-  async setOne(idx: number, updateOrderDto: UpdateOrderDto) {
+  async update(code: string, updateOrderDto: UpdateOrderDto): Promise<boolean> {
     try {
-      const order: UpdateOrderDto = await this.getOne(idx);
-
-      console.log(updateOrderDto);
-
-      const code = updateOrderDto.code ? updateOrderDto.code : order.code;
-      const order_no = updateOrderDto.order_no
-        ? updateOrderDto.order_no
-        : order.order_no;
-      const site_code = updateOrderDto.site_code
-        ? updateOrderDto.site_code
-        : order.site_code;
-      const user_code = updateOrderDto.user_code
-        ? updateOrderDto.user_code
-        : order.user_code;
-      const created_time = updateOrderDto.created_time
-        ? updateOrderDto.created_time
-        : order.created_time;
-      const updated_time = updateOrderDto.updated_time
-        ? updateOrderDto.updated_time
-        : order.updated_time;
-      const delivered_time = updateOrderDto.delivered_time
-        ? updateOrderDto.delivered_time
-        : order.delivered_time;
+      const con = await this.databaseService.getConnection();
+      const existingData = (
+        await con.query(`
+      SELECT 
+      post_number,
+      receiver_name,
+      receiver_address,
+      receiver_phone,
+      receiver_phone2,
+      total_price FROM ${this.configService.get(
+        'DB_NAME'
+      )}.order WHERE code="${code}";
+      `)
+      )[0][0];
       const post_number = updateOrderDto.post_number
         ? updateOrderDto.post_number
-        : order.post_number;
+        : existingData.post_number;
       const receiver_name = updateOrderDto.receiver_name
         ? updateOrderDto.receiver_name
-        : order.receiver_name;
+        : existingData.receiver_name;
       const receiver_address = updateOrderDto.receiver_address
         ? updateOrderDto.receiver_address
-        : order.receiver_address;
+        : existingData.receiver_address;
       const receiver_phone = updateOrderDto.receiver_phone
         ? updateOrderDto.receiver_phone
-        : order.receiver_phone;
+        : existingData.receiver_phone;
       const receiver_phone2 = updateOrderDto.receiver_phone2
         ? updateOrderDto.receiver_phone2
-        : order.receiver_phone2;
+        : existingData.receiver_phone2;
       const total_price = updateOrderDto.total_price
         ? updateOrderDto.total_price
-        : order.total_price;
+        : existingData.total_price;
 
-      return this.databaseService.query(`
-      UPDATE testA.order
+      await con.query(`
+      UPDATE ${this.configService.get('DB_NAME')}.order
       SET
-      code='${code}',
-      order_no='${order_no}',
-      site_code='${site_code}',
-      user_code='${user_code}',
-      created_time='${created_time}',
-      updated_time='${updated_time}',
-      delivered_time='${delivered_time}',
       post_number='${post_number}',
       receiver_name='${receiver_name}',
       receiver_address='${receiver_address}',
       receiver_phone='${receiver_phone}',
       receiver_phone2='${receiver_phone2}',
       total_price= '${total_price}'
-      WHERE id=${idx};
+      WHERE code="${code}";
       `);
+      return true;
     } catch (error) {
       throw error;
     }
   }
   /**
    * 주문 삭제 함수
-   * @param idx idx값으로 찾습니다.
+   * @param code code값으로 찾습니다.
    * @returns 리턴값은 없습니다.
    */
-  async remove(idx: number) {
+  async remove(code: string): Promise<boolean> {
     try {
-      await this.databaseService.query(`
-      DELETE from testA.order
-      WHERE id=${idx}
+      const con = await this.databaseService.getConnection();
+      await con.query(`
+      DELETE from ${this.configService.get('DB_NAME')}.order
+      WHERE code="${code}";
       `);
+      return true;
     } catch (error) {
       throw error;
     }
